764fd62db4b7a4ac090bae1647b655149c123542
gempir <gempir.dev@gmail.com>
2020-11-20 21:33:58 +0100

    use bash file to run

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-/usr/bin/flock -n /tmp/***REMOVED***.lockfile /usr/bin/python3 /root/***REMOVED***/main.py
+/usr/bin/flock -n /tmp/***REMOVED***.lockfile /root/***REMOVED***/run.sh
