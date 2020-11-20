357b5622edb4d1123def07036734a79b24b00cd5
gempir <gempir.dev@gmail.com>
2020-11-20 21:38:17 +0100

    use pid file

--- run.sh
+++ run.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash
 
-/usr/bin/flock -n /tmp/***REMOVED***.lockfile /root/***REMOVED***/run.sh
+mkdir -p "/tmp"
+PIDFILE="/tmp/***REMOVED***.pid"
+
+if [ -e "${PIDFILE}" ] && (ps -u $(whoami) -opid= |
+                           grep -P "^\s*$(cat ${PIDFILE})$" &> /dev/null); then
+  echo "Already running."
+  exit 99
+fi
+
+/root/***REMOVED***/run.sh >> /root/cron.log &
+
+echo $! > "${PIDFILE}"
+chmod 644 "${PIDFILE}"
