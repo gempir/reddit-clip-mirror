3209f4615e675ea981368325d27c42e4ff0bc2ad
gempir <gempir.dev@gmail.com>
2021-11-05 21:16:41 +0100

    improved v2 bot

---
+++ Makefile
@@ -0,0 +1,15 @@
+.PHONY: run setup
+include .env
+
+export STREAMABLE_EMAIL
+export STREAMABLE_PASSWORD
+export REDDIT_CLIENT_ID
+export REDDIT_CLIENT_SECRET
+export REDDIT_USERNAME
+export REDDIT_PASSWORD
+
+run:
+	python3 main.py
+
+setup:
+	pip3 install -r requirements.txt
--- main.py
+++ main.py
@@ -1,42 +1,28 @@
-import os.path
-from os import path
-import re
 import logging
+import os
+import re
 
-import json
 import praw
-
 import requests
 
 logging.basicConfig(
-    filename='process.log',
     format='%(asctime)s %(levelname)-8s %(message)s',
     level=logging.INFO,
     datefmt='%Y-%m-%d %H:%M:%S')
 
-def load_json(file):
-    with open(file) as json_file:
-        return json.load(json_file)
-
-
-cfg = load_json("config.json")
-
 reddit = praw.Reddit(
-    client_id=cfg['clientId'],
-    client_secret=cfg['clientSecret'],
-    user_agent=cfg['userAgent'],
-    username=cfg['username'],
-    password=cfg['password']
+    client_id=os.getenv('REDDIT_CLIENT_ID'),
+    client_secret=os.getenv('REDDIT_CLIENT_SECRET'),
+    user_agent="botnextdoor by /u/gempir github.com/gempir/reddit-clip-mirror",
+    username=os.getenv('REDDIT_USERNAME'),
+    password=os.getenv('REDDIT_PASSWORD')
 )
-
-regex = r"https:\/\/(www|clips)\.twitch\.tv\/(\w*\/)?(clip\/)?\w*"
-
 
 def check_condition(c):
     text = c.body
 
     match = re.search(
-        r"https:\/\/(www|clips)\.twitch\.tv\/(\w*\/)?(clip\/)?\w*", text)
+        r"\bhttps?:\/\/((clips|m)\.twitch\.tv\/?\/?(clip)?|www\.twitch\.tv\/([A-z\d_]+)?\/clip)\/[A-z]+(-[A-z\d_-]{16})?\b", text)
     if match and match.group():
         return match.group()
     else:
@@ -44,25 +30,26 @@ def check_condition(c):
 
 
 def main():
-    postsid = ['jxr1p8', 'jxqvez', 'jxrmcl', 'jxrn3p', 'jxr0sb', 'jxqoip',
-               'jxraoe', 'jxqnwf', 'jxqgr8', 'jxqkpt', 'jxqjhh', 'jxqgj4', 'jxqers']
+    postsid = ['qmm84u']
+    logging.info(f"Scanning {len(postsid)} posts")
 
     for post in postsid:
         submission = reddit.submission(id=post)
         submission.comments.replace_more(limit=None)
 
-        logging.info("Processing {} comments".format(len(submission.comments)))
+        logging.info(f"Processing {len(submission.comments)} comments on post {submission.name}")
         for c in submission.comments:
             url = check_condition(c)
             already_replied = False
 
             if len(c.replies) > 0:
                 for reply in c.replies:
-                    if reply.author.name == cfg['username']:
+                    if reply.author.name == os.getenv('REDDIT_USERNAME'):
                         already_replied = True
                         break
 
             if url and not already_replied:
+                logging.info(f"Found new clip: {url}")
                 link = uploadToStreamable(url)
                 if link:
                     logging.info("Replying {}: Mirror: {}".format(c.id, link))
@@ -72,14 +59,14 @@ def main():
 def uploadToStreamable(url):
     try:
         response = requests.get('https://api.streamable.com/import?url=' +
-                                url, auth=(cfg['streamableEmail'], cfg['streamablePassword']))
+                                url, auth=(os.getenv('STREAMABLE_EMAIL'), os.getenv('STREAMABLE_PASSWORD')))
         if response.status_code == 200:
             data = response.json()
             logging.info("Success uploading: " + url +
                          " id: " + data['shortcode'])
             return "http://streamable.com/" + data['shortcode']
         else:
-            logging.error("{} {}".format(response.status_code, response.text))
+            logging.error("Streamable error {} {}".format(response.status_code, response.text))
             return False
     except requests.exceptions.RequestException as e:
         logging.error(e)
