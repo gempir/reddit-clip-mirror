8b431aadea611f11fa8fd1f0871de3b77d46c614
gempir <gempir.dev@gmail.com>
2021-12-03 19:41:51 +0100

    less local

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,7 +15,7 @@ do
     file=$(echo $line | cut -f1 -d";")
     comment=$(echo $line | cut -f2 -d";")
 
-    mega-put -v "./$file" nnys2021clips/clips
+    mega-put -v "$file" nnys2021clips/clips
     link=`mega-export -a "nnys2021clips/$file" | rev | cut -d ' ' -f 1 | rev`
     python3 reply.py "$comment" "${link#'https://'}"
 done < "replies.txt"
