b6b061c78779606ed566eaba7360b2b9fa54012c
gempir <gempir.dev@gmail.com>
2021-12-03 18:45:26 +0100

    escape var

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,18 +4,18 @@ set -e
 
 cd /app
 
+bash
+
 python3 main.py
 
 mega-login "$MEGA_USERNAME" "$MEGA_PASSWORD"
-
-
 
 while IFS= read -r line
 do
     file=$(echo $line | cut -f1 -d";")
     comment=$(echo $line | cut -f2 -d";")
 
-    mega-put "./$file" nnys2021clips
-    link=`mega-export -a nnys2021clips/$file`
+    mega-put -v "./$file" nnys2021clips/clips
+    link=`mega-export -a "nnys2021clips/$file"`
     python3 reply.py "$comment" "$link"
 done < "replies.txt"
