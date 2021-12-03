8ebfde86e11760ff031f8b2c53672b1cf531c3cf
gempir <gempir.dev@gmail.com>
2021-12-03 17:43:15 +0100

    run individually

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,12 +8,14 @@ python3 main.py
 
 mega-login "$MEGA_USERNAME" "$MEGA_PASSWORD"
 
-mega-put ./clips nnys2021clips
+
 
 while IFS= read -r line
 do
     file=$(echo $line | cut -f1 -d";")
     comment=$(echo $line | cut -f2 -d";")
+
+    mega-put "./$file" nnys2021clips
     link=`mega-export -a nnys2021clips/$file`
     python3 reply.py "$comment" "$link"
 done < "replies.txt"
